a44a1debe8ba9e39d2e1e641563da1367c1ad418
Chandan <Chandankumarck1005@gmail.com>
2024-08-31 07:14:13 +0530

    adding styling and logic

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const apiKey = '90f45d47b08fb4bc4b8d307fc0dfc59e';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-
+const container=document.querySelector(".container");
+const body=document.querySelector("body");
 const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
 const locationElement = document.getElementById('location');
+const latitude=document.querySelector(".lat");
+const longtitude=document.querySelector(".lon");
+const weatherdeatils=document.querySelector(".hide");
+const error1=document.querySelector(".error");
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 
@@ -11,6 +16,7 @@ searchButton.addEventListener('click', () => {
     const location = locationInput.value;
     if (location) {
         fetchWeather(location);
+        // weatherdeatils.classList.add("hide");
     }
 });
 
@@ -20,11 +26,30 @@ function fetchWeather(location) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            locationElement.textContent = data.name;
-            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-            descriptionElement.textContent = data.weather[0].description;
+            console.log(data);
+            locationElement.textContent =`Location: ${data.name}, ${(data.sys.country)}`;
+            latitude.textContent=`latitude:  ${data.coord.lat}`;
+            longtitude.textContent=`Longtitude: ${data.coord.lon}`;
+            temperatureElement.textContent = `temperature: ${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = `Clouds:  ${data.weather[0].description}`;
+            if(data.main.temp>23){
+                container.classList.add("sun");
+                container.classList.remove("rain");
+                body.classList.add("sunb");
+                body.classList.remove("rainb");
+            }
+            else{
+                container.classList.add("rain");
+                container.classList.remove("sun");
+                body.classList.add("rainb");
+                body.classList.remove("sunb");
+            }
+            weatherdeatils.classList.remove("hide");
+            error1.classList.add("error");
         })
         .catch(error => {
-            console.error('Error fetching weather data:', error);
+            error1.innerText='Error fetching weather data:', error;
+            error1.classList.remove("error");
+            weatherdeatils.classList.add("hide");
         });
 }
